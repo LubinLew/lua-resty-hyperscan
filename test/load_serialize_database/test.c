906dd7e7d62e69edbeabb80d88442cbe840ad48f
lubinlew <lgbxyz@gmail.com>
2021-02-01 16:56:22 +0800

    update

--- test/load_serialize_database/test.c
+++ test/load_serialize_database/test.c
@@ -23,71 +23,71 @@ static int eventHandler(unsigned int id, unsigned long long from,
 
 int main(void)
 {
-	hs_error_t ret;
-	hs_database_t* db;
+    hs_error_t ret;
+    hs_database_t* db;
     hs_scratch_t *scratch = NULL;
-	char* info;
-	
-	int fd;
-	void* db_ptr;
-	struct stat sb;
+    char* info;
 
-	if((fd = open(DB_STORE_PATH, O_RDONLY)) < 0){
-		perror("open");
-		return -1;
-	}
-	
-	if((fstat(fd, &sb)) == -1 ){
-		perror("fstat");
-		return -1;
-	}	
-	 
-	db_ptr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if(MAP_FAILED == db_ptr){
-		perror("mmap");
-		return -1;
-	}
+    int fd;
+    void* db_ptr;
+    struct stat sb;
 
-	ret = hs_serialized_database_info(db_ptr, sb.st_size, &info);
-	if (ret != HS_SUCCESS) {
+    if((fd = open(DB_STORE_PATH, O_RDONLY)) < 0){
+        perror("open");
+        return -1;
+    }
+
+    if((fstat(fd, &sb)) == -1 ){
+        perror("fstat");
+        return -1;
+    }
+
+    db_ptr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if(MAP_FAILED == db_ptr){
+        perror("mmap");
+        return -1;
+    }
+
+    ret = hs_serialized_database_info(db_ptr, sb.st_size, &info);
+    if (ret != HS_SUCCESS) {
         fprintf(stderr, "ERROR: hs_serialized_database_info failed: %s\n", info);
-		munmap(db_ptr, sb.st_size);
-		close(fd);
-		hs_free_database(db);
-		return -1;
-	}
-	// OUTPUT:Version: 5.3.0 Features: AVX2 Mode: BLOCK
-	fprintf(stdout, "SUCCESS: hs_serialized_database_info: %s\n", info);
+        munmap(db_ptr, sb.st_size);
+        close(fd);
+        hs_free_database(db);
+        return -1;
+    }
+    // OUTPUT:Version: 5.3.0 Features: AVX2 Mode: BLOCK
+    fprintf(stdout, "SUCCESS: hs_serialized_database_info: %s\n", info);
 
-	
-	ret = hs_deserialize_database(db_ptr, sb.st_size, &db);
-	if (ret != HS_SUCCESS) {
+
+    ret = hs_deserialize_database(db_ptr, sb.st_size, &db);
+    if (ret != HS_SUCCESS) {
         fprintf(stderr, "ERROR: serialize database failed: %d\n", ret);
-		munmap(db_ptr, sb.st_size);
-		close(fd);
-		hs_free_database(db);
-		return -1;
-	}
+        munmap(db_ptr, sb.st_size);
+        close(fd);
+        hs_free_database(db);
+        return -1;
+    }
 
-	munmap(db_ptr, sb.st_size);
-	close(fd);
+    munmap(db_ptr, sb.st_size);
+    close(fd);
 
     if (hs_alloc_scratch(db, &scratch) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to allocate scratch space. Exiting.\n");
         hs_free_database(db);
         return -1;
-	}
+    }
 
-	ret = hs_scan(db, TARGET_STRING, sizeof(TARGET_STRING)-1, 0, scratch, eventHandler, NULL);
-	if (ret != HS_SUCCESS && ret != HS_SCAN_TERMINATED) {
+    ret = hs_scan(db, TARGET_STRING, sizeof(TARGET_STRING)-1, 0, scratch, eventHandler, NULL);
+    if (ret != HS_SUCCESS && ret != HS_SCAN_TERMINATED) {
         fprintf(stderr, "ERROR: hs_scan faild, %d\n", ret);
         hs_free_database(db);
-	}
-	
-	hs_free_scratch(scratch);
-	hs_free_database(db);
+    }
 
-	return 0;
+    hs_free_scratch(scratch);
+    hs_free_database(db);
+
+    return 0;
 }
 
 
--- test/vectored_scan/test.c
+++ test/vectored_scan/test.c
@@ -7,29 +7,29 @@
 #include <hs.h>
 
 const char * pattern[] = {
-	"123456",
-	"hello",
-	"\\d{5}"
+    "123456",
+    "hello",
+    "\\d{5}"
 };
 
 const unsigned int ids[] = {
-	1001, 1002, 1003
+    1001, 1002, 1003
 };
 
 typedef struct _str {
-	char*  data;
-	size_t len;
+    char*  data;
+    size_t len;
 }str_t;
 
 #define strelm(a) {a, sizeof(a)-1}
 
 static str_t inputdata[] = {
-	strelm("ABChelo"),
-	strelm("555 world"),
-	strelm("xccx333@@@"),
-	strelm("666sssa123"),
-	strelm("dddxxx45"),
-	strelm("1345563444")
+    strelm("ABChelo"),
+    strelm("555 world"),
+    strelm("xccx333@@@"),
+    strelm("666sssa123"),
+    strelm("dddxxx45"),
+    strelm("1345563444")
 };
 
 
@@ -42,49 +42,49 @@ static int eventHandler(unsigned int id, unsigned long long from,
 
 int main(void)
 {
-	int i = 0;
-	hs_database_t* db;
-	hs_compile_error_t* compile_err;
-	unsigned int elements = sizeof(pattern)/sizeof(pattern[0]); 
-	hs_error_t ret;
+    int i = 0;
+    hs_database_t* db;
+    hs_compile_error_t* compile_err;
+    unsigned int elements = sizeof(pattern)/sizeof(pattern[0]);
+    hs_error_t ret;
     hs_scratch_t *scratch = NULL;
-	unsigned int* length;
-	unsigned int count;
-	const char** data;
-	
-	ret = hs_compile_ext_multi(pattern, NULL, ids, NULL, elements, 
-    					HS_MODE_VECTORED, NULL, &db, &compile_err);
-	if (ret) {
+    unsigned int* length;
+    unsigned int count;
+    const char** data;
+
+    ret = hs_compile_ext_multi(pattern, NULL, ids, NULL, elements,
+                        HS_MODE_VECTORED, NULL, &db, &compile_err);
+    if (ret) {
         fprintf(stderr, "ERROR: Unable to compile pattern: %s\n", compile_err->message);
         hs_free_compile_error(compile_err);
         return -1;
-	}
+    }
 
     if (hs_alloc_scratch(db, &scratch) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to allocate scratch space. Exiting.\n");
         hs_free_database(db);
         return -1;
-	}
-	
-	count = sizeof(inputdata)/sizeof(str_t);
-	length = calloc(sizeof(unsigned int), count);
-	data = calloc(sizeof(char*), count);
-	for (i = 0; i < count; i++) {
-		data[i] = inputdata[i].data;
-		length[i] = inputdata[i].len;
-	}
-	ret = hs_scan_vector(db, data, length, count, 0, scratch, eventHandler, NULL);
-	if (ret != HS_SUCCESS && ret != HS_SCAN_TERMINATED) {
-		fprintf(stderr, "ERROR: Unable to scan input buffer. Exiting.\n");
-		hs_free_scratch(scratch);
-		hs_free_database(db);
-		return -1;
-	}
-	
-	hs_free_scratch(scratch);
-	hs_free_database(db);
+    }
 
-	return 0;
+    count = sizeof(inputdata)/sizeof(str_t);
+    length = calloc(sizeof(unsigned int), count);
+    data = calloc(sizeof(char*), count);
+    for (i = 0; i < count; i++) {
+        data[i] = inputdata[i].data;
+        length[i] = inputdata[i].len;
+    }
+    ret = hs_scan_vector(db, data, length, count, 0, scratch, eventHandler, NULL);
+    if (ret != HS_SUCCESS && ret != HS_SCAN_TERMINATED) {
+        fprintf(stderr, "ERROR: Unable to scan input buffer. Exiting.\n");
+        hs_free_scratch(scratch);
+        hs_free_database(db);
+        return -1;
+    }
+
+    hs_free_scratch(scratch);
+    hs_free_database(db);
+
+    return 0;
 }
 
 
